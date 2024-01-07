cdd6eda33158d5374e5ac7a8ea3ecafb994e3a10
njugunamwangi <njugunamwangi.n@gmail.com>
2024-01-07 14:08:48 +0300

    Department  livewire

---
+++ app/Livewire/DepartmentForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class DepartmentForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.department-form');
+    }
+}
